0418b35c823f3e9f94c1e50bc8f08dc33aef5a5d
Masanov Jumabek <masanov3167@gmail.com>
2023-06-23 12:32:13 +0500

    cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,19 +2,19 @@ module.exports = [
   "strapi::errors",
   "strapi::security",
   "strapi::poweredBy",
-  {
-    name: "strapi::cors",
-    config: {
-      origin: ["*"],
-      methods: ["GET"],
-      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
-      keepHeaderOnError: true,
-    },
-  },
   "strapi::logger",
   "strapi::query",
   "strapi::body",
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["https://madeira.netlify.app"],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+      keepHeadersOnError: true,
+    },
+  },
 ];
