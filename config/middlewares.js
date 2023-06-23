7a8ee6aa132af54125f5ed1e61af353e10be8c97
Masanov Jumabek <masanov3167@gmail.com>
2023-06-23 12:17:29 +0500

    update

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,6 +2,14 @@ module.exports = [
   "strapi::errors",
   "strapi::security",
   "strapi::poweredBy",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: ["http://localhost:1337", "https://madeira.netlify.app"],
+    },
+  },
   "strapi::logger",
   "strapi::query",
   "strapi::body",
