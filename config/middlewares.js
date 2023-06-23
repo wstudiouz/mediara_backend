e15428fdd2272f5c49ecd2eaeb00ef2dcf601b31
Masanov Jumabek <masanov3167@gmail.com>
2023-06-23 11:44:53 +0500

    add cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,18 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["https://madeira.netlify.app"],
+      methods: ["GET"],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
