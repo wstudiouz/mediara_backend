18442e5da47efcb34fea0d62ccc3847575523bee
Masanov Jumabek <masanov3167@gmail.com>
2023-06-24 21:41:39 +0500

    ok

--- config/server.js
+++ config/server.js
@@ -1,10 +1,8 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("URL", "http://13.53.127.78:1337"), // Update the URL to match your desired endpoint
-  admin: {
-    auth: {
-      secret: env("ADMIN_JWT_SECRET", "a1b2c3d4e5f6g7h8i9j0"), // Update the secret key if needed
-    },
+  app: {
+    keys: env.array("APP_KEYS"),
   },
+  url: env("URL", "https://madeira.netlify.app"),
 });
